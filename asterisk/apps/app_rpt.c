17014e8782451945517cc185f4265cbb0a1b39c2
Steve Zingman <N4IRS@users.noreply.github.com>
2021-05-31 06:03:36 -0400

    Add missing test. Thanks to George, W2DB

--- asterisk/apps/app_rpt.c
+++ asterisk/apps/app_rpt.c
@@ -4380,6 +4380,7 @@ static int retrieve_memory(struct rpt *myrpt, char *memory)
 				myrpt->remmode = REM_MODE_LSB;
 				break;
 			case 'F':
+			case 'f':	
 				myrpt->remmode = REM_MODE_FM;
 				break;
 			case 'L':
